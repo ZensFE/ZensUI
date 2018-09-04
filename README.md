68512fafe448bcf27a2d838278b614dd5e9d6546
wangchong <idoodle@aliyun.com>
2018-09-04 16:40:19 +0800

    section： 优化.btn.pill追加、demo完善
     1. 根据rpx()封装的结果修改了_button.scss
     2. 修改了demo中的示例
     3. .pill和.radius重新划分任务

--- README.md
+++ README.md
@@ -3,6 +3,23 @@
 
 这是一个为【哲品】开发的前端UI框架
 
+<p align="left">
+  <a href="https://npmcharts.com/compare/zens-ui?minimal=true"><img src="https://img.shields.io/npm/dm/zens-ui.svg" alt="Downloads"></a>
+  <a href="https://www.npmjs.com/package/zens-ui"><img src="https://img.shields.io/npm/v/zens-ui.svg" alt="Version"></a>
+  <a href="https://www.npmjs.com/package/zens-ui"><img src="https://img.shields.io/npm/l/zens-ui.svg" alt="License"></a>
+</p>
+
+### 快速上手
+安装
+```bash
+npm install --save-dev zens-ui
+```
+引入
+```javascript
+import 'zens-ui'
+```
+
+
 ## 开发脚本
 ```bash
 npm run dev
--- demo/app.vue
+++ demo/app.vue
@@ -1,27 +1,40 @@
-
 <template>
-    <div class="all-container">
-        <h3>色值系统</h3>
+  <div class="all-container">
+    <h3>色值系统</h3>
 
-        <h4>字体颜色</h4>
-        <ul>
-            <li class="text-primary">主色 primary</li>
-            <li class="text-white">白色 white</li>
-            <li class="text-dark">深色 dark</li>
-        </ul>
+    <h4>字体颜色</h4>
+    <ul>
+      <li class="text-primary">主色 primary</li>
+      <li class="text-white">白色 white</li>
+      <li class="text-dark">深色 dark</li>
+    </ul>
 
-        <h4>按钮</h4>
-        <div class="button-list">
-            <button class="btn radius" >主色按钮</button>
-            <button class="btn radius primary reverse">主色翻转按钮</button>
-            <button class="btn radius primary hollow">主色镂空按钮</button>
-            <button class="btn radius red hollow">红色镂空按钮</button>
-            <button class="btn red radius">红色按钮</button>
-            <button class="btn red radius reverse">红色翻转按钮</button>
-            <button class="btn small radius">小按钮</button>
-            <button class="btn big radius" :class="test">大按钮 </button>
-        </div>
+    <h4>按钮</h4>
+    <div class="button-list">
+
+      <h5>按钮大小</h5>
+      <button class="btn radius small">small 按钮</button>
+      <button class="btn radius base">base 按钮</button>
+      <button class="btn radius big">big 按钮</button>
+
+      <h5>按钮颜色</h5>
+      <button class="btn radius">主色按钮</button>
+      <button class="btn radius primary reverse">主色翻转按钮</button>
+      <button class="btn radius primary hollow">主色镂空按钮</button>
+      <button class="btn radius red hollow">红色镂空按钮</button>
+      <button class="btn red radius">红色按钮</button>
+      <button class="btn red radius reverse">红色翻转按钮</button>
+
+      <h5>按钮形状</h5>
+      <button class="btn">默认长方形</button>
+      <button class="btn radius">圆角按钮</button>
+      <button class="btn pill">椭圆按钮</button>
+
     </div>
+
+
+    <h4></h4>
+  </div>
 
 </template>
 <script>
@@ -43,7 +56,7 @@
         background-color: $light;
     }
     .button-list button{
-        width: 80%;
+        width: 200px;
         margin: 5px 0;
     }
 </style>
